66925e78fc9975bc615c28df8d901d287fc804a5
andreisoroka <a.soroka@semrush.com>
2016-09-02 21:51:00 +0300

    rename some function in class User

--- application/classes/users.js
+++ application/classes/users.js
@@ -65,7 +65,7 @@ class Users {
         for (let key in people) {
             let peopleRole = roles.splice(0, 1);
             peopleRole = peopleRole.concat(this._rolesAlliace[peopleRole[0]]);
-            people[key].group = peopleRole;
+            people[key].role = peopleRole;
         }
 
         this._gameIsStarted = true;
@@ -81,15 +81,15 @@ class Users {
     _generateMapUsers() {
         this._peoplesName = {};
         this._peoplesId = {};
-        this._peoplesGroup = {};
+        this._peoplesRole = {};
         this._peoples.forEach((people)=> {
             // для быстрого поиска со
             this._peoplesName[people.name] = people;
             this._peoplesId[people.id] = people;
-            people.group.forEach((role)=> {
-                (this._peoplesGroup[role])
-                    ? this._peoplesGroup[role].push(people)
-                    : this._peoplesGroup[role] = [people];
+            people.role.forEach((role)=> {
+                (this._peoplesRole[role])
+                    ? this._peoplesRole[role].push(people)
+                    : this._peoplesRole[role] = [people];
             });
         });
     }
@@ -106,12 +106,12 @@ class Users {
         if (!role) return -1;
 
         if (people) {
-            people.group = role;
+            people.role = role;
         }
         else if (id || name) {
             let _people = this.get({id, name});
             if (_people) {
-                _people.group = role;
+                _people.role = role;
             }
             else {
                 return -2
@@ -129,17 +129,17 @@ class Users {
      * Поиск пользователя/пользователей
      * @param {number} [id] по Id пользователя
      * @param {number} [name] по нику пользователя
-     * @param {number} [group] по группе пользователя
+     * @param {number} [role] по группе пользователя
      */
-    get({id, name, group}) {
+    get({id, name, role}) {
         if (id) {
             return this._peoplesId && this._peoplesId[id];
         }
         if (name) {
             return this._peoplesName && this._peoplesName[name];
         }
-        if (group) {
-            return this._peoplesGroup && this._peoplesGroup[group];
+        if (role) {
+            return this._peoplesRole && this._peoplesRole[role];
         }
         return this._peoples;
     }
